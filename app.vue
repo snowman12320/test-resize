bd050a9fcb71aac9467e27e15df0b2adddcba322
snowman12320 <snowman12320@gmail.com>
2024-02-29 15:24:32 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    style="
+      height: 500px;
+      width: 500px;
+      border: 1px solid red;
+      position: relative;
+    "
+  >
+    <vue-draggable-resizable :w="100" :h="100" :parent="true">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+    </vue-draggable-resizable>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import VueDraggableResizable from 'vue-draggable-resizable';
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 100%;
+  height: 100%;
+}
+</style>
