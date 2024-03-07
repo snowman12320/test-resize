75c10e420c48657b8f8008c4c1e9926eac0e22ef
snowman12320 <snowman12320@gmail.com>
2024-03-07 09:12:56 +0800

    Add test dependencies and scripts

---
+++ components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup>
+// const newsTicker = ref([]);
+
+// const getFetchPageUserMessage = (perPage: number, nowPage: number) => {
+//   useNuxtApp()
+//     .$frontend.cannedMessage.fetchPageUserMessage(
+//       perPage,
+//       nowPage,
+//       member.token,
+//       getClientLang()
+//     )
+//     .then((res) => {
+//       const { data } = res;
+//       newsTicker.value = data;
+//       return data;
+//     })
+//     .catch((e: AxiosError) => {
+//       console.log(e);
+//       throw e;
+//     });
+// };
+// getFetchPageUserMessage(15, 1);
+</script>
+
+<style lang="scss" scoped>
+
+</style>
